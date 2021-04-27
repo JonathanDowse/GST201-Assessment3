de7a4a3dcd047a6d1e1828f2b037cb2f41706c4e
Jonathan Dowse <jonathan.dowse@mds.torrens.edu.au>
2021-04-27 12:23:25 +1000

    Second commit
    Added:
    -Scenes
    -Pause menu capability
    -Navigational Buttons
    -Imported sprites
    ...

---
+++ GST/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+            }
+
+            else
+            {
+                Time.timeScale = 1;
+                gamePaused = false;
+                Cursor.visible = false;
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+
+   
+}
